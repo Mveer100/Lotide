02555ba37a5b913e7137f9f8344927e698c272e3
Maxwell Veer <maxaveer@gmail.com>
2022-03-09 19:24:57 +0000

    added the middle function to lotide

--- middle.js
+++ middle.js
@@ -9,6 +9,8 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
+
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     return console.log(`Assertion Passed: ${arr1} === ${arr2}`);
@@ -16,3 +18,18 @@ const assertArraysEqual = function(arr1, arr2) {
     return console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
+
+
+const middle = function (array) {
+  let middleArr = []
+  if (array.length / 2 <= 1) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[array.length/2-1], array[array.length/2]]
+  } else if (array.length % 2 !== 0) {
+    return [array[array.length/2-.5]]
+  }
+}
+
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
